ce0f4a44ff032590fb893d90645f6c4e8cfb29f5
K NANDINI <kooranandini@gmail.com>
2025-08-21 23:14:29 +0530

    reset-password

--- routes/auth.routes.js
+++ routes/auth.routes.js
@@ -1,11 +1,21 @@
 const express = require("express");
-const { registerController, loginController, logoutController } = require("../controllers/auth.controller.js");
+const {
+  registerController,
+  loginController,
+  logoutController,
+  forgotPasswordController,
+  resetPasswordController
+} = require("../controllers/auth.controller.js");
 const isAuthenticated = require("../middlewares/isAuthenticated.middleware.js");
 
 const authRouter = express.Router();
 
 authRouter.post("/register", registerController);
 authRouter.post("/login", loginController);
-authRouter.get("/logout",isAuthenticated, logoutController);
+authRouter.get("/logout", isAuthenticated, logoutController);
+
+// Forgot password routes
+authRouter.post("/forgot-password", forgotPasswordController); // email with token
+authRouter.post("/reset-password/:token", resetPasswordController); // reset password
 
 module.exports = authRouter;
